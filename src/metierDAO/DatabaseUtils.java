fae0b05b20e8c96cd5311174085657d12674e977
Damien <Damien@Damien-Portable>
2015-01-12 12:04:31 +0100

    tu suces ?

--- src/metierDAO/DatabaseUtils.java
+++ src/metierDAO/DatabaseUtils.java
@@ -2,9 +2,9 @@ package metierDAO;
 
 public class DatabaseUtils {
 	
-	public static final String BASE = ""; // Mettre entre " " le nom de la base
+	public static final String BASE = "6quiprend"; // Mettre entre " " le nom de la base
 	
-	public static final String TABLE_COMPTE = " "; // Mettre entre " " le nom de la base
+	public static final String TABLE_COMPTE = "user"; // Mettre entre " " le nom de la table
 	
 	// ici faut remplir avec les noms de colonne qui sont dans la table Compte
 	
