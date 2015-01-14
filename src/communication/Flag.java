b80d42edfd5855a4856c5d44a06395ca3af7e4c3
JulienMrgrd <julien.margarido@gmail.com>
2015-01-14 22:45:37 +0100

    la classe Flag

---
+++ src/communication/Flag.java
@@ -0,0 +1,18 @@
+package communication;
+
+public class Flag {
+	
+	public static final int CREATION_PARTIE = 0;
+	public static final int REFRESH_LIST_PARTIES = 1;
+	public static final int REJOINDRE_PARTIE = 2;
+	public static final int PARTIE_COMMENCE = 3;
+	public static final int SEND_CARTE = 4;
+	public static final int TROP_TARD_POUR_CARTE = 5;
+	public static final int SEND_LIGNE = 6;
+	public static final int TROP_TARD_POUR_LIGNE = 7;
+	public static final int REFRESH_BEEF = 8;
+	public static final int CARTE_ADVERSAIRES = 9;
+	public static final int GAGNANT = 10;
+	public static final int PERDANT = 11;
+	
+}
