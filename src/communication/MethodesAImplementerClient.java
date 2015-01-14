d2983044b81f2f89c2a9d85589443f6ca2011357
JulienMrgrd <julien.margarido@gmail.com>
2015-01-14 22:45:03 +0100

    Les flags et les méthodes à implémenter

--- src/communication/MethodesAImplementerClient.java
+++ src/communication/MethodesAImplementerClient.java
@@ -5,64 +5,88 @@ import metier.User;
 
 public class MethodesAImplementerClient {
 
-	// La classe Client contient une instance de FenetrePrincipale fp;
+	// La classe Client contient une instance de FenetrePrincipale fp, et un user (qui sera initialisé lors de l'authentification);
+	//	Client c = null;
+	//	try {
+	//		c = new Client( new URI( "ws://localhost:12345" ));
+	//	} catch (URISyntaxException e1) {
+	//		e1.printStackTrace();
+	//	}
+	//	final FenetrePrincipale f = new FenetrePrincipale(c);
+	
+	
 	// La classe Serveur contient la liste des parties;
 	
 	
 	// TODO dans Client.java : le user doit informer le serveur qu'il vient de créer une partie.
 	// TODO dans Serveur.java : l'ajoute à sa liste des parties (récupère donc ce qu'il y a sur le flux, construit un new Partie, et add dans sa liste).
 	public void creationPartie(String nom,int nbMaxJoueur,boolean modePro, User user){
-		//socket.write(#FLAG CREATION PARTIE:nom:nbMaxJoueur,modePro, user)
+		//socket.write(Flag.CREATION_PARTIE: nom,nbMaxJoueur,modePro, user)
 	}
 	
 	
-	// TODO DANS Client.java : lorsque le user choisit de rejoindre une partie, il doit demander au serveur si je peux la rejoindre
-	// et me retourne vrai ou faux.
+	// TODO DANS Client.java : lorsque le user choisit de rejoindre une partie, il doit demander au serveur si il peux la rejoindre
+	// et attend vrai ou faux.
 	// TODO dans Serveur.java, si ce user peut rejoindre la partie (voir avec Patrick/Nourdine pour la verif), 
-	// il l'ajoute dans la partie, et retourne vrai.
+	// il l'ajoute dans la partie, et retourne vrai, sinon faux.
 	public void rejoindrePartie(Partie idPartie, User user){
-		//socket.write(#FLAG REJOINDRE PARTIE: idPartie, user)
+		//socket.write(Flag.REJOINDRE_PARTIE : idPartie, user)
 		// return bool serait niquel, sinon faut voir dans l'écoute (voir en bas)
 	}
 	
-	// TODO dans Client.java : envoie au serveur la valeur d'une carte, avec un flag contenant l'id de la partie
+	// TODO dans Client.java : envoie au serveur la valeur d'une carte, avec un Flag contenant l'id de la partie
 	// TODO dans Serveur.java : reçoit la carte, et la file à la partie en cours (partie sait quelle carte correspond à quel joueur)
 	public void sendCarteChoisie(int idPartie, int carteValue){
-		//socket.write(#FLAG ENVOYER CARTE PARTIE: idPartie : carteValue)
+		//socket.write(Flag.SEND_CARTE: idPartie : carteValue)
 	}
 	
-	// TODO dans Client.java : envoie au serveur la valeur d'une carte, avec un flag contenant l'id de la partie
-	// TODO dans Serveur.java : reçoit la carte, et la file à la partie en cours (partie sait quelle carte correspond à quel joueur)
-	public void sendLigneChoisie(int idPartie, int numero){
-		//socket.write(#FLAG ENVOYER CARTE PARTIE: idPartie : carteValue)
+	// TODO dans Client.java : envoie au serveur la valeur d'une ligne, avec un Flag contenant l'id de la partie
+	// TODO dans Serveur.java : reçoit la ligne, et la file à la partie en cours (partie sait quelle carte correspond à quel joueur)
+	public void sendLigneChoisie(int idPartie, int ligneValue){
+		//socket.write(Flag.SEND_LIGNE : idPartie : ligneValue)
 	}
 	
-	// rafraichir
+	// TODO dans Client.java : l'affichage demande au serveur de renvoyer la liste des parties (et attend)
+	// TODO dans Serveur.java : reçoit la demande, et renvoie avec le même Flag la liste des parties (avec uniquement les valeurs
+	// utiles pour l'affichage)
+	public void rafraichirListeParties(){
+		// socket.write(Flag.REFRESH_LIST_PARTIES);
+	}
 	
 	
 	public void ecoute(){
-		// Ecoute ce que le serveur dit
-		// switch(flag reçu){
+		// Ecoute ce que le serveur dit, reçoit, la plupart du temps, un Flag suivi de données.
+		// 
+		// switch(Flag reçu){
 		//
-		// case FLAG.REJOINDRE_PARTIE :
-		//			l'interface fp attend un boolean
-		// case FLAG.RAFRAICHIR:
-		//			l'interface fp attend une liste de Partie
-		// case FLAG.PARTIE_COMMENCE
-		// 			l'interface attend qu'on lui dise que la partie commence
-		// case FLAG.DONNE_TA_CARTE
-		//			l'interface doit appeler la méthode sendCarteChoisie(idPartie,valueCarte)
-		// case FLAG.TROP_TARD_POUR_CARTE
-		//			l'interface doit sendCarteChoisie avec une carte aléatoire
-		// case FLAG.DONNE_LIGNE
-		//			l'interface doit appeler la méthode sendLigneChoisie(idPartie,numLigne)
-		// case FLAG.TROP_TARD_POUR_LIGNE
-		//			l'interface doit sendLigneChoisie avec une carte aléatoire
+		// case Flag.REFRESH_LIST_PARTIES:
+		//			donner la liste des parties à l'affichage
+		// case Flag.REJOINDRE_PARTIE :
+		//			donne un boolean a l'affichage pour l'autoriser à rejoindre la partie
+		// case Flag.PARTIE_COMMENCE
+		// 			préviens l'affichage que la partie commence
+		// case Flag.SEND_CARTE
+		//			fp.setTropTardCarte(false);
+		//			demande a l'affichage de choisir une carte
+		// case Flag.TROP_TARD_POUR_CARTE
+		//			fp.setTropTardCarte(true);
+		//			dit à l'affichage qu'il est trop tard pour envoyer une carte
+		// case Flag.SEND_LIGNE
+		//			fp.setTropTardLigne(false);
+		//			demande à l'affichage de choisir une carte
+		// case Flag.TROP_TARD_POUR_LIGNE
+		//			fp.setTropTardLigne(true);
+		//			dit a l'affichage qu'il est trop tard pour envoyer une carte
+		// case Flag.REFRESH_BEEF:
+		//			donne a l'affichage sa nouvelle tete de boeuf
+		// case Flag.CARTE_ADVERSAIRES:
+		//			donne a l'affichage la carte jouée par chaque adversaire
+		// case Flag.GAGNANT:
+		//			donne a l'affichage le ou les gagnants
+		// case Flag.PERDANT:
+		//			donne a l'affichage le perdant.
 		
 	}
-	
-	
-	
 	
 	
 }
--- src/communication/MethodesAImplementerServeur.java
+++ src/communication/MethodesAImplementerServeur.java
@@ -1,5 +1,90 @@
 package communication;
 
-public class MethodesAImplementerServeur {
+import java.util.List;
 
+import metier.Partie;
+
+public class MethodesAImplementerServeur {
+	
+	// La classe Serveur contient la liste des parties;
+	List<Partie> parties;
+	
+	// La classe Client contient une instance de FenetrePrincipale fp;
+	
+	
+	//	envoie au client concerné la liste des parties
+	public void refreshListParties(List<Partie> parties){ 
+		// socket.write(Flag.REFRESH_LIST_PARTIES:parties)
+	}
+	
+	// envoie au client concerné la réponse à sa demande pour rejoindre une partie
+	public void rejoindrePartie(boolean reponse){
+		// socket.write(Flag.REJOINDRE:reponse)
+	}
+	
+	// demande a tous les joueurs de la partie d'envoyer une carte
+	public void sendCarte(){
+		// socket.write(Flag.SEND_CARTE);
+	}
+	
+	// demande a tous les joueurs de la partie d'envoyer une ligne
+	public void sendLigne(){
+		// socket.write(Flag.SEND_LIGNE);
+	}
+	
+	// dit au joueur concerné que c'est trop tard pour envoyer une carte
+	public void sendTroTardCarte(){
+		// socket.write(Flag.TROP_TARD_POUR_CARTE);
+	}
+	
+	// dit au joueur concerné que c'est trop tard pour envoyer une ligne
+	public void sendTropTardLigne(){
+		// socket.write(Flag.TROP_TARD_POUR_LIGNE);
+	}
+	
+	// donne au client concerné les tetes de boeufs à ajouter pour son user
+	public void refreshBeef(int nbBeef){
+		// socket.write(Flag.REFRESH_BEEF: nbBeef);
+	}
+	
+	// affiche aux clients de la partie le ou les gagnants
+	public void afficheGagnant(String ... nomGagnant){
+		// socket.write(Flag.GAGNANT:nomGagnant[0],nomGagnant[1],...);
+	}
+	
+	// affiche aux clients de la partie le perdant
+	public void affichePerdants(String nomPerdant){
+		// socket.write(Flag.GAGNANT:nomPerdant);
+	}
+			
+	
+	public void ecoute(){
+		
+		// Ecoute ce que le client dit. Reçoit, la plupart du temps, un Flag suivi de données.
+		// 
+		// switch(Flag reçu){
+		//
+		// case Flag.CREATION_PARTIE:
+		//		- ajoute à sa liste de partie la partie reçu avec ce flag (faire new Partie avec les paramètres reçus)
+		
+		// case Flag.REJOINDRE_PARTIE :
+		//		- verifie si il reste une place
+		//		- si oui, ajoute le user à la partie et effectue this.rejoindrePartie(true)
+		//		- si non, this.rejoindrePartie(false);
+		
+		// case Flag.SEND_CARTE
+		//		- retrouver la partie graçe au paramètre reçu idPartie
+		//		- faire new Carte avec la valeur reçue
+		//		- ajouter à la partie la carte (il sait quelle carte correspond à quel joueur)
+		
+		
+		// case Flag.SEND_LIGNE
+		//		- retrouver la partie graçe au paramètre reçu idPartie
+		//		- ajouter à la partie le int reçu
+		
+		
+		// case Flag.REFRESH_LISTE_PARTIES:
+		//		- this.refreshListeParties(parties);
+		
+	}
 }
