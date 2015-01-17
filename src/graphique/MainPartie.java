f6a3d2218bf32ffd87edca5cd876e432303de9ea
PatpatP <pol.patrick1411@gmail.com>
2015-01-17 16:29:44 +0100

    Main to test a partie

--- src/graphique/MainPartie.java
+++ src/graphique/MainPartie.java
@@ -1,19 +1,37 @@
 package graphique;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import metier.Partie;
+import communication.User;
+
 
 public class MainPartie {
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) throws URISyntaxException{
+
+		User user = new User(new URI("ws://localhost:12345"));
+		user.setUser(1, "Patrick");
+
+		Partie game = new Partie("DANT Game", 2, false, user.getUserNickname());
+		game.start();
+
+		User user2 = new User(new URI("ws://localhost:12345"));
+		user.setUser(2, "Nourdine");
 		
-////		User user = new User("Nourdine", "nourdine@gmail.com", "ghansum");
-////		User user2 = new User("Patrick", "patrick@gmail.com", "pol");
-//		Partie partie = new Partie("Partie1", 2, false, user);
-//		partie.addPlayer(user2);
-//		
-//		System.out.println("Nom de la partie : "+partie.getNom());
-//		System.out.println("Nombre de joueurs : "+partie.getNbJoueursMax());
-//		System.out.println("****************** debut de la partie ***************\n");
-//		
-//		partie.startGame();
+		try {
+			synchronized (game) {
+				Thread.sleep(5000);
+				System.out.println("End of sleep");
+				game.addPlayer(user2.getUserNickname());
+				System.out.println(game.getListUser().size());
+				game.notify();
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
